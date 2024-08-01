de4b60b238305face51115eba2346ceb7e3b9869
dima19-93 <dima70593@ukr.net>
2024-08-01 23:08:11 +0300

    Create swap.py
    swap.py

---
+++ swap.py
@@ -0,0 +1,14 @@
+#Информация о размере свап файла
+import psutil
+def size_swap():
+    swap = psutil.swap_memory()
+    return swap.__init__
+
+swap = size_swap()
+print(f"Размер свап файла: {bytes} байт")
+def memory():
+    memory = psutil.boot_time()
+
+
+echo "Hello friend"
+
