e189491bdfcb41c8becc8921168ea890a8ebbf85
dima19-93 <dima70593@ukr.net>
2024-08-01 23:11:27 +0300

    Create processes.py
    processes.py

---
+++ processes.py
@@ -0,0 +1,11 @@
+  #Просмотр инормации о запущенных процессах
+running_processes = running_processes
+print (f"Запущенные процессы: {len(running_processes)}")
+fdsfdfdsdsf
+dfsdfdsaf
+dsafds
+fdsf
+
+
+
+
