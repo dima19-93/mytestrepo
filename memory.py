1ac18206b073cee391e9826eec1067fa9adbb9ce
dima19-93 <dima70593@ukr.net>
2024-08-01 23:09:58 +0300

    Create memory.py
    memory.py

---
+++ memory.py
@@ -0,0 +1,13 @@
+#Просмотр инормации о занятой ОЗУ
+def size_memory():
+  memory = psutil.swap_memory()
+  return memory.__init__
+memory = size_memory()
+print(f"Занятость оперативной памяти: {bytes} байт")
+def memory():
+  running_processes = ["name", "surname"]
+  memory = psutil.boot_time()
+
+
+#dsfdfdsfdsfdsfdsfeqaewqewq23213
+
